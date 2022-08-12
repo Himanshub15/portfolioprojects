92499c1a22b5a32a6866c47c3b0bc884a1ee4f01
Himanshub15 <70678684+Himanshub15@users.noreply.github.com>
2022-08-12 11:03:58 -0500

    Add files via upload

---
+++ da_project.sql
@@ -0,0 +1,172 @@
+-- select * from coviddeaths order by 3,4;
+
+-- select * from covidvaccinations order by 3,4
+
+select location, date, total_cases, new_cases, total_deaths, population 
+from coviddeaths order by 1,2;
+
+-- Looking at total cases vs total deaths
+select location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as DeathPercentage
+from coviddeaths where location like "%states%"
+order by 1,2 ASC;
+
+-- Looking at Total cases vs population
+select location, date, population, total_cases, (total_cases/population) * 100 as percentofpopulation
+from coviddeaths where location like "%states%"
+order by 1,2 ASC;
+
+-- Country has highest infection rate
+select location, population, MAX(total_cases) as highestinfectioncount, MAX((total_cases/population)) * 100 as percentofpopulation
+from coviddeaths 
+-- where location like "%states%"
+group by population, location
+order by 1,2 ASC;
+
+-- Showing total deaths per countries
+select location, population, MAX(cast(total_deaths as signed)) as totaldeathscount
+from coviddeaths 
+-- where location like "%states%"
+where continent is not null
+group by location, population
+order by 1,2 ASC;
+
+-- Break things down by continent
+select continent, MAX(cast(total_deaths as signed)) as totaldeathscount
+from coviddeaths 
+-- where location like "%states%"
+where continent != ''
+group by continent
+order by totaldeathscount DESC;
+
+-- showing contnent with highest death rate
+select continent, MAX(cast(total_deaths as signed)) as totaldeathscount
+from coviddeaths 
+-- where location like "%states%"
+where continent != ''
+group by continent
+order by totaldeathscount DESC;
+
+-- global numbers 
+select date,Sum(new_cases) as total_cases, SUM(cast(new_deaths as signed)) as total_deaths, SUM(cast(new_deaths as signed))/ SUM(NEw_cases) *100 as deathpercentage 
+from coviddeaths 
+-- where location like "%states%"
+where continent is not null
+group by date, date
+order by 1,2 ;
+
+--  JOIN
+select *
+FROM coviddeaths dea 
+join covidvaccinations vac 
+	on dea.location = vac.location
+	and dea.date = vac.date;
+    
+-- Looking for total population 
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+from coviddeaths dea Join covidvaccinations vac
+	on dea.location = vac.location 
+    and dea.date = vac.date
+where dea.continent is not null
+order by 2,3;  
+
+-- Looking for total population vs population 
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(convert(vac.new_vaccinations, signed)) 
+over (partition by dea.location order by dea.location, dea.date)
+-- (rollingpeoplevaccinated / population )*100
+from coviddeaths dea Join covidvaccinations vac
+	on dea.location = vac.location 
+    and dea.date = vac.date
+Where dea.continent is not null
+order by 2,3;
+
+-- USE CTE
+
+With popvsvac (contitnet, Location, date, population, New_vaccinations, Rollingpeoplevaccinated) 
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(convert(vac.new_vaccinations, signed)) 
+over (partition by dea.location order by dea.location, dea.date)
+-- (rollingpeoplevaccinated / population )*100
+from coviddeaths dea Join covidvaccinations vac
+	on dea.location = vac.location 
+    and dea.date = vac.date
+Where dea.continent != ''
+-- order by 2,3
+)
+Select *, (Rollingpeoplevaccinated/Population) *100
+From popvsvac;
+
+-- Temp table
+Drop table if exists portfolio_project.temp1;
+
+Create temporary table portfolio_project.temp1
+
+(
+continent nvarchar(255),
+Location nvarchar (2255),
+population numeric,
+new_vaccination numeric,
+Rollingpeoplevaccinated numeric
+);
+
+Insert into portfolio_project.temp1
+Select dea.continent, dea.location, dea.population, vac.new_vaccinations, sum(convert(vac.new_vaccinations, signed)) 
+over (partition by dea.location order by dea.location, dea.date)
+-- (rollingpeoplevaccinated / population )*100
+from coviddeaths dea Join covidvaccinations vac
+	on dea.location = vac.location 
+    and dea.date = vac.date
+    where vac.new_vaccinations != '';
+-- Where dea.continent != ''
+-- order by 2,3
+
+Select *, (Rollingpeoplevaccinated/Population) *100
+From portfolio_project.temp1;
+
+-- creating view
+create view Portfolio_Project.percentpopulation as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(convert(vac.new_vaccinations, signed)) 
+over (partition by dea.location order by dea.location, dea.date)
+-- (rollingpeoplevaccinated / population )*100
+from coviddeaths dea Join covidvaccinations vac
+	on dea.location = vac.location 
+    and dea.date = vac.date
+Where dea.continent != '';
+
+
+
+-- Queries used for Tableau Project
+ -- 1
+ 
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as signed)) as total_deaths, SUM(cast(new_deaths as signed))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+-- Where location like '%states%'
+where continent != '' 
+-- Group By date
+order by 1,2;
+ 
+ -- 2
+
+Select location, SUM(cast(new_deaths as signed)) as TotalDeathCount
+From CovidDeaths
+-- Where location like '%states%'
+Where continent = '' 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc;
+
+-- 3
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Portfolio_Project.CovidDeaths
+-- Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+-- 4 
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Portfolio_Project.CovidDeaths
+-- Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
